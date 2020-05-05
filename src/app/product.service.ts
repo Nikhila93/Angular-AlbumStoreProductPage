2b5de33a63e79b38739ae715f00044b65bf235e3
Nikhila <nikhila112293@gmail.com>
2020-05-05 16:14:03 -0600

    Product List Component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -12,9 +13,13 @@ export class ProductService {
   ) { }
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json'
 
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
-
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response=> <Product[]>response.json());
+  }
+  
 }
