7fa6fffb5479d093a0dc5d9ef47a054f889f38e0
Nikhila <nikhila112293@gmail.com>
2020-05-05 14:53:09 -0600

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+
+@Injectable()
+export class ProductService {
+
+  constructor(
+    private _http: Http
+  ) { }
+
+  private _albumUrl = '../assets/album.json';
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl);
+  }
+
+}
